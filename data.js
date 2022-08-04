52e4d69dad38eae09d63c4efe9041340a34bab40
tnsatt <44207684+tnsatt@users.noreply.github.com>
2022-08-04 23:38:51 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,6 @@
 let data=[
-  ["https://images.unsplash.com/photo-1522865080725-2a9ea1fcb94e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max"]
+  [
+    "https://i.pinimg.com/564x/3a/4a/46/3a4a46e382196097c77551092d644d28.jpg",
+    "https://i.pinimg.com/564x/40/79/8f/40798f031bb66ddb2609651bcdb20026.jpg",
+  ]
 ];
