34bc9075874aea539663c2479f181b333711c6a8
tnsatt <44207684+tnsatt@users.noreply.github.com>
2022-08-05 01:25:31 +0700

    Update data.js

--- data.js
+++ data.js
@@ -2,5 +2,9 @@ let data=[
   [
     "https://i.pinimg.com/564x/3a/4a/46/3a4a46e382196097c77551092d644d28.jpg",
     "https://i.pinimg.com/564x/40/79/8f/40798f031bb66ddb2609651bcdb20026.jpg",
-  ]
+  ],
+  [
+      "https://i.pinimg.com/564x/59/3f/3f/593f3f41742406f3f5ae224db5bbf214.jpg",
+      "https://i.pinimg.com/564x/e1/65/b4/e165b4838d877f83618554d98345b49f.jpg",
+  ],
 ];
